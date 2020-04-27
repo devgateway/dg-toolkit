298774701f23171efcc2376fdc83a41aba42bda4
mpostelnicu <mpostelnicu@gmail.com>
2020-04-27 19:56:23 +0300

    fixes #264

--- persistence/src/main/java/org/devgateway/toolkit/persistence/service/BaseJpaServiceImpl.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/service/BaseJpaServiceImpl.java
@@ -18,89 +18,73 @@ import java.util.Optional;
  * @author idobre
  * @since 2019-03-04
  */
-@CacheConfig(keyGenerator = "genericKeyGenerator", cacheNames = "servicesCache")
 public abstract class BaseJpaServiceImpl<T extends GenericPersistable & Serializable> implements BaseJpaService<T> {
 
     @Override
-    @Cacheable
     public List<T> findAll() {
         return repository().findAll();
     }
 
     @Override
-    @Cacheable
     public List<T> findAll(final Sort sort) {
         return repository().findAll(sort);
     }
 
     @Override
-    @Cacheable
     public List<T> findAll(final Specification<T> spec) {
         return repository().findAll(spec);
     }
 
     @Override
-    @Cacheable
     public Page<T> findAll(final Specification<T> spec, final Pageable pageable) {
         return repository().findAll(spec, pageable);
     }
 
     @Override
-    @Cacheable
     public Optional<T> findOne(final Specification<T> spec) {
         return repository().findOne(spec);
     }
 
     @Override
-    @Cacheable
     public Page<T> findAll(final Pageable pageable) {
         return repository().findAll(pageable);
     }
 
     @Override
-    @Cacheable
     public List<T> findAll(final Specification<T> spec, final Sort sort) {
         return repository().findAll(spec, sort);
     }
 
     @Override
-    @Cacheable
     public long count(final Specification<T> spec) {
         return repository().count(spec);
     }
 
     @Override
-    @Transactional(readOnly = false)
-    // @Cacheable - no need for cache here.
+    @Transactional
     public Optional<T> findById(final Long id) {
         return repository().findById(id);
     }
 
-    @Cacheable
-    public Optional<T> findByIdCached(final Long id) {
-        return repository().findById(id);
-    }
-
     @Override
-    @Cacheable
     public long count() {
         return repository().count();
     }
 
     @Override
-    @Transactional(readOnly = false)
+    @Transactional
     public <S extends T> S save(final S entity) {
         return repository().save(entity);
     }
 
     @Override
-    @Transactional(readOnly = false)
+    @Transactional
     public <S extends T> S saveAndFlush(final S entity) {
         return repository().saveAndFlush(entity);
     }
 
     @Override
-    @Transactional(readOnly = false)
+    @Transactional
     public void delete(final T entity) {
         repository().delete(entity);
     }
