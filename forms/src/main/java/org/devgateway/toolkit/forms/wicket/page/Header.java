e032d47610129907f993a70c7082727393d5f992
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-10-20 15:24:56 +0300

    display reboot alert

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/Header.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/Header.java
@@ -11,8 +11,24 @@
  *******************************************************************************/
 package org.devgateway.toolkit.forms.wicket.page;
 
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AbstractAjaxTimerBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.authroles.authorization.strategies.role.metadata.MetaDataRoleAuthorizationStrategy;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.devgateway.toolkit.forms.security.SecurityConstants;
+import org.devgateway.toolkit.persistence.dao.AdminSettings;
+import org.devgateway.toolkit.persistence.service.AdminSettingsService;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 /**
  * @author idobre
@@ -22,11 +38,68 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 public class Header extends Panel {
     private static final long serialVersionUID = 1L;
 
+    private static final Duration ONE_MIN = Duration.ofMinutes(1);
+    private static final long ALERT_UPDATE_INTERVAL_SECONDS = 15;
+
+    @SpringBean
+    private AdminSettingsService adminSettingsService;
+
+    private LocalDateTime rebootSince;
+
     public Header(final String markupId) {
         this(markupId, null);
     }
 
     public Header(final String markupId, final PageParameters parameters) {
         super(markupId);
+
+        addRebootAlert();
+    }
+
+    private void addRebootAlert() {
+        AdminSettings as = adminSettingsService.get();
+        if (as == null) {
+            add(new WebMarkupContainer("rebootAlert"));
+            return;
+        }
+        rebootSince = as.getRebootAlertSince();
+
+        IModel<String> rebootDurationModel = new IModel<String>() {
+            private static final long serialVersionUID = -8601598474017148336L;
+
+            @Override
+            public String getObject() {
+                if (rebootSince == null) {
+                    return "";
+                } else {
+                    Duration remaining = AdminSettings.REBOOT_ALERT_DURATION
+                            .minus(Duration.between(rebootSince, LocalDateTime.now()))
+                            // round up
+                            .plusSeconds(30);
+                    if (remaining.minus(ONE_MIN).isNegative()) {
+                        remaining = ONE_MIN;
+                    }
+                    return DurationFormatUtils.formatDuration(remaining.toMillis(), "m");
+                }
+            }
+        };
+
+        Label rebootAlert = new Label("rebootAlert", new StringResourceModel("rebootAlert", rebootDurationModel));
+        rebootAlert.setOutputMarkupId(true).setOutputMarkupPlaceholderTag(true);
+        rebootAlert.setVisible(as.isRebootServer());
+        add(rebootAlert);
+        MetaDataRoleAuthorizationStrategy.authorize(rebootAlert, Component.RENDER, SecurityConstants.Roles.ROLE_USER);
+
+        add(new AbstractAjaxTimerBehavior(org.apache.wicket.util.time.Duration.seconds(ALERT_UPDATE_INTERVAL_SECONDS)) {
+            private static final long serialVersionUID = -1168209018766325709L;
+
+            @Override
+            protected void onTimer(final AjaxRequestTarget target) {
+                AdminSettings as = adminSettingsService.get();
+                rebootSince = as.getRebootAlertSince();
+                rebootAlert.setVisible(as.isRebootServer());
+                target.add(rebootAlert);
+            }
+        });
     }
 }
--- persistence/src/main/java/org/devgateway/toolkit/persistence/service/AdminSettingsServiceImpl.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/service/AdminSettingsServiceImpl.java
@@ -4,9 +4,11 @@ import org.devgateway.toolkit.persistence.dao.AdminSettings;
 import org.devgateway.toolkit.persistence.repository.AdminSettingsRepository;
 import org.devgateway.toolkit.persistence.repository.norepository.BaseJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author idobre
@@ -26,6 +28,34 @@ public class AdminSettingsServiceImpl extends BaseJpaServiceImpl<AdminSettings> 
     @Override
     public AdminSettings newInstance() {
         return new AdminSettings();
+    }
+
+    @Override
+    @Transactional
+    public <S extends AdminSettings> S save(final S entity) {
+        preProcessRebootAlert(entity);
+        return repository().save(entity);
+    }
+
+    @Override
+    @Transactional
+    public <S extends AdminSettings> S saveAndFlush(final S entity) {
+        preProcessRebootAlert(entity);
+        return repository().saveAndFlush(entity);
+    }
+
+    private <S extends AdminSettings> void preProcessRebootAlert(final S as) {
+        if (!as.isRebootServer()) {
+            as.setRebootAlertSince(null);
+        } else if (as.getRebootAlertSince() == null) {
+            as.setRebootAlertSince(LocalDateTime.now());
+        }
+    }
+
+    @Override
+    public AdminSettings get() {
+        List<AdminSettings> entries = repository().findAll();
+        return entries.isEmpty() ? null :  entries.get(0);
     }
 
 }
