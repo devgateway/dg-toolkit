15a56ab692c8ecfdba0460b533e3771e2cfcbdeb
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2021-08-03 23:30:57 +0300

    keep status filter directly in JpaFilterState that can be added on demand

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/components/table/filter/TestFormFilterState.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/components/table/filter/TestFormFilterState.java
@@ -3,6 +3,7 @@ package org.devgateway.toolkit.forms.wicket.components.table.filter;
 import org.apache.commons.lang3.StringUtils;
 import org.devgateway.toolkit.persistence.dao.TestForm;
 import org.devgateway.toolkit.persistence.dao.TestForm_;
+import org.devgateway.toolkit.persistence.repository.SpecificationContext;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Predicate;
@@ -12,7 +13,7 @@ import java.util.List;
 /**
  * Created by Octavian on 03.07.2016.
  */
-public class TestFormFilterState extends AbstractStatusFilterState<TestForm> {
+public class TestFormFilterState extends JpaFilterState<TestForm> {
 
     private static final long serialVersionUID = 8005371716983257722L;
     private String textField;
@@ -21,7 +22,9 @@ public class TestFormFilterState extends AbstractStatusFilterState<TestForm> {
     public Specification<TestForm> getSpecification() {
         return (root, query, cb) -> {
             List<Predicate> predicates = new ArrayList<>();
-            addStatus(predicates, root, query, cb);
+            SpecificationContext<TestForm> sc = new SpecificationContext<>(root, query, cb);
+
+            addStatus(predicates, sc);
 
             if (StringUtils.isNotBlank(textField)) {
                 predicates.add(cb.like(root.get(TestForm_.textField), "%" + textField + "%"));
